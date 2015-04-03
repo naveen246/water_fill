ce1a0f6f542778f1534a9d935bb70f0d202a9b2b
Naveen <naveen@keepworks.com>
2015-04-04 01:54:05 +0530

    static line working

--- app/src/main/java/com/example/naveen/customviews/WaterFill.java
+++ app/src/main/java/com/example/naveen/customviews/WaterFill.java
@@ -2,7 +2,9 @@ package com.example.naveen.customviews;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -13,19 +15,49 @@ import com.example.naveen.waterfill.R;
  */
 public class WaterFill extends View {
 
-    int mColor;
+    int waterColor;
+    Paint mSurfaceLinePaint = new Paint();
+    Paint mWaterPaint = new Paint();
 
     public WaterFill(Context context, AttributeSet attrs) {
         super(context, attrs);
-        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.WaterFill, 0, 0);
 
+        parseAttributes(context.obtainStyledAttributes(attrs, R.styleable.WaterFill));
+        setupPaints();
+    }
+
+    public int getColor() {
+        return waterColor;
+    }
+
+    public void setColor(int waterColor) {
+        this.waterColor = waterColor;
+        invalidate();
+        requestLayout();
+    }
+
+    private void parseAttributes(TypedArray a) {
         try {
-            mColor = a.getColor(R.styleable.WaterFill_waterColor, Color.BLUE);
+            waterColor = a.getColor(R.styleable.WaterFill_waterColor, Color.BLUE);
         } finally {
             a.recycle();
         }
     }
 
-    
+    private void setupPaints() {
+        mSurfaceLinePaint.setColor(waterColor);
+        mSurfaceLinePaint.setAntiAlias(true);
+        mSurfaceLinePaint.setStyle(Paint.Style.STROKE);
+        mSurfaceLinePaint.setStrokeWidth(2);
 
+        mWaterPaint.setColor(waterColor);
+        mWaterPaint.setAntiAlias(true);
+        mWaterPaint.setStyle(Paint.Style.FILL);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawLine(0, 0, 50, 50, mSurfaceLinePaint);
+    }
 }
